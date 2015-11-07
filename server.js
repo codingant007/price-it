4383adf4151f93dc33b4ff19487a7554e59ce30e
Nikhil Alamanda <ant.nikhil96@gmail.com>
2015-11-08 02:54:12 +0530

    sql test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
-//var qs = require('query-string');
-//var url = require('url');
+var qs = require('query-string');
+var url = require('url');
 
 
 var port = process.env.PORT || 1337;
