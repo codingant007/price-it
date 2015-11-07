a778feb323d61b12150130064381d8e8789dd2b7
Nikhil Alamanda <ant.nikhil96@gmail.com>
2015-11-07 21:24:51 +0530

    node do or die

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var http = require('http');
-var https = require('https');
-
-var port = process.env.PORT || 8080;
-
+var port = process.env.PORT || 1337;
 http.createServer(function(req,res) {
 	res.writeHead(200, {'Content-Type':'text/html'});
 	res.end('Hello World\n');
 }).listen(port);
-console.log('Server running at http://localhost:8080');
