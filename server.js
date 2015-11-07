eadfdc0825375586749cbb7b14dda7b71844666e
Nikhil Alamanda <ant.nikhil96@gmail.com>
2015-11-08 04:30:02 +0530

    sql test

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var http = require('http');
 var qs = require('query-string');
 var url = require('url');
 
+var Connection = require('tedious').Connection;
+var config = {
+    userName: 'fsociety',
+    password: 'HelloWorld1',
+    server: 'c0pmp50v6q.database.windows.net',
+    // If you are on Microsoft Azure, you need this:
+    options: {encrypt: true, database: 'geoprix'}
+};
 
 var port = process.env.PORT || 1337;
 http.createServer(function(req,res) {
