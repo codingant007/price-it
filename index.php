0befb22f7ebe372280dd1542393ac27e88bd58d8
Nikhil Alamanda <ant.nikhil96@gmail.com>
2015-11-07 19:27:01 +0530

    php test

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <title>Hello!</title>  		
   </head>
   <body>
+  <?php
+  		echo "Hello from php!";
+  ?>
     <h1>Hello !</h1>
     <button type="button" id="auth-button" type="button" >click</button>
   </body>
